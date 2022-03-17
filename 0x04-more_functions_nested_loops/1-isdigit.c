0d90f2a451718d64d5492acf41a55c1a0e65e25f
Carmel Prosper SAGBO <sagbocarmel@gmail.com>
2022-03-18 00:22:44 +0100

    4 more function

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,10 @@
+#include "main.h"
+
+int _isdigit(int c)
+{
+	if (c > 47 && c < 58)
+	{
+		return (1);
+	}
+	return (0);
+}
---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,10 @@
+#include "main.h"
+
+/**
+ * Description: Multiply check function_isupper
+ * Return: a * b
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
