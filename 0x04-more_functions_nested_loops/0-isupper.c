5a28039e411eff2f2f3a03fc41139db1e98cb139
Carmel Prosper SAGBO <sagbocarmel@gmail.com>
2022-03-17 21:51:01 +0100

    is uppeer test

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,15 +1,15 @@
 #include "main.h"
 
 /**
- * _isupper - Check if an int value is upper or not
- * Description : UpperCase check function
- * Return : 0 or 1
-*/
-// int _isupper(int c)
-// {
-//	if (c > 64 && c < 91)
-//	{
-//		return (1);
-//	}
-//	return (0);
-//}
+ * Description: UpperCase check function_isupper
+ * Check if an int value is upper or not
+ * Return: 0 or 1
+ */
+int _isupper(int c)
+{
+	if (c > 64 && c < 91)
+	{
+		return (1);
+	}
+	return (0);
+}
--- 0x04-more_functions_nested_loops/0-main.c
+++ 0x04-more_functions_nested_loops/0-main.c
@@ -4,8 +4,9 @@
 
 /**
  * main - entry point
+ *
  * Return: Always 0.
-*/
+ */
 int main(void)
 {
 	char c;
