b9d96b12fb1f71d4d91f1901d12ec90e733fe526
Carmel Prosper SAGBO <sagbocarmel@gmail.com>
2022-03-17 03:58:15 +0100

    Putchar Nested

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+ * print_alphabet
+ * Description : Print the Alphabet
+ * Return : Always void 
+ *
+*/
+void print_alphabet(void)
+{
+	char ch;
+
+	for(ch = 'a' ; ch <= 'a' ; ch++) {
+		putchar(ch);
+	}
+   	putchar('\n');
+}
+
+/**
+ * main - Entry point
+ * Description: Print Alphabet
+ * Return: Always 0.
+ *
+*/
+int main(void)
+{
+	printf("_putchar\n");
+	return (0);
+}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,6 +1,12 @@
 #ifndef MAIN_H
 #define MAIN_H
 #include <stdio.h>
-
+/**
+ * print_alphabet
+ * Description : Print the Alphabet
+ * Return : Always void 
+ *
+*/
+void print_alphabet(void);
 
 #endif
