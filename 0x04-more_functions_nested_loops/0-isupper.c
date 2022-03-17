e56ae17720cf7729da93fcdf3b1174f8749bffa9
Carmel Prosper SAGBO <sagbocarmel@gmail.com>
2022-03-17 21:48:19 +0100

    is uppeer test

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -5,11 +5,11 @@
  * Description : UpperCase check function
  * Return : 0 or 1
 */
-int _isupper(int c)
-{
-	if (c > 64 && c < 91)
-	{
-		return (1);
-	}
-	return (0);
-}
+// int _isupper(int c)
+// {
+//	if (c > 64 && c < 91)
+//	{
+//		return (1);
+//	}
+//	return (0);
+//}
