ae8c6eb2a59958f7650a667ab69d4860524720e7
Carmel Prosper SAGBO <sagbocarmel@gmail.com>
2022-03-18 04:52:02 +0100

    ok

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -3,14 +3,13 @@
 /**
  * print_numbers - function that print all numbers from
  * 0 to 9
- *
  * Return: returns nothing
  */
 void print_numbers(void)
 {
 	int i;
-	
-	for (i = 48; i <= 57; i++)
+
+	for (i = 48; i < 58; i++)
 	{
 		_putchar(i);
 	}
