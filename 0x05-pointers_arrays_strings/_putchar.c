4af761f1830872f2575451038ddb3ec5a5220ccb
Carmel Prosper SAGBO <sagbocarmel@gmail.com>
2022-03-22 01:16:20 +0100

    All scripts written

---
+++ 0x05-pointers_arrays_strings/_putchar.c
@@ -0,0 +1,12 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _putchar - Used to print only char
+ * @c: input
+ * Return: void
+ */
+void _putchar(int c)
+{
+	putchar(c);
+}
--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,5 +1,6 @@
 #ifndef MAIN_H
 #define MAIN_H
+#include <stdio.h>
 
 /**
 * void prototypes()
@@ -10,6 +11,7 @@ void reset_to_98(int *n);
 void swap_int(int *a, int *b);
 int _strlen(char *s);
 void _puts(char *str);
+void _putchar(int c);
 void print_rev(char *s);
 void rev_string(char *s);
 void puts2(char *str);
