5bc53fd5ad8da7ce39c3b7416ffa2c3abc8266a2
Carmel Prosper SAGBO <sagbocarmel@gmail.com>
2022-03-25 03:03:38 +0100

    ok

--- 0x06-pointers_arrays_strings/104-print_buffer.c
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -21,7 +21,7 @@ void print_buffer(char *b, int size)
 
 	if (size < 0)
 	{
-		printf('\n');
+		printf("\n");
 		return;
 	}
 
@@ -47,7 +47,7 @@ void print_buffer(char *b, int size)
 			else
 				printf(".");
 		}
-		printf('\n');
+		printf("\n");
 		i += 10;
 	}
 }
--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -17,6 +17,7 @@ char *_strncpy(char *dest, char *src, int n);
 char *string_toupper(char *);
 char *cap_string(char *);
 char transform(char x);
+void _putchar(int c);
 char *leet(char *);
 char *rot13(char *);
 char *infinite_add(char *n1, char *n2, char *r, int size_r);
