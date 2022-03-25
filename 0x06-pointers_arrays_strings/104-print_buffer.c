fbf43dee904408a9cfa80a79a69ed6e854981d8e
Carmel Prosper SAGBO <sagbocarmel@gmail.com>
2022-03-25 03:41:28 +0100

    ok

--- 0x06-pointers_arrays_strings/104-print_buffer.c
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -29,15 +29,17 @@ void print_buffer(char *b, int size)
 	{
 		if (i % 10 == 0)
 			printf("%08x: ", i);
-		for (j = i; j <= i + 9; j += 2)
+		for (j = i; j < i + 9; j += 2)
 		{
 			if ((j < size) && ((j + 1) < size))
 				printf("%02x%02x: ", b[j], b[j + 1]);
 			else
 			{
 				while (++j <= i + 10)
+				{
 					printf(" ");
-				printf(" ");
+				}
+				printf("	");
 			}
 		}
 		for (j = i; j <= i + 9 && j < size; j++)
