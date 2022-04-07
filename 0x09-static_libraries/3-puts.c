19d701327e71787d3e46aa5cdbc0fd40b322513b
Carmel Prosper SAGBO <sagbocarmel@gmail.com>
2022-04-07 03:26:49 +0100

    Static lib created

--- 0x09-static_libraries/3-puts.c
+++ 0x09-static_libraries/3-puts.c
@@ -1,21 +1,20 @@
-#include <stdio.h>
 #include "main.h"
 
 /**
  * _puts - prints a string
- * @str: string to print
+ * @s: string to print
  *
  * Description: prints a string
  * On success: returns no error
  */
 
-void _puts(char *str)
+void _puts(char *s)
 {
 	int i = 0;
 
-	while (*(str + i) != '\0')
+	while (*(s + i) != '\0')
 	{
-		_putchar(*(str + i));
+		_putchar(*(s + i));
 		i++;
 	}
 	_putchar(10);
