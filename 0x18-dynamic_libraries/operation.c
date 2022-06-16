3cd7a703469a168a3151434d5c2023b5dc93a485
Carmel Prosper SAGBO <sagbocarmel@gmail.com>
2022-06-16 23:20:32 +0100

    Dynamic librairy

---
+++ 0x18-dynamic_libraries/operation.c
@@ -0,0 +1,57 @@
+#include "operation.h"
+
+/**
+ * mul - sum two numbers
+ * @a: first number
+ * @b: second number
+ * Return: returns result
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * mul - substract two numbers
+ * @a: first number
+ * @b: second number
+ * Return: returns result
+ */
+int sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * mul - multiplies two numbers
+ * @a: first number
+ * @b: second number
+ * Return: returns result
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * div - divides two numbers
+ * @a: first number
+ * @b: second number
+ * Return: returns result
+ */
+int div(int a, int b)
+{
+	return (a/b);
+}
+
+
+/**
+ * mod - modulo operation
+ * @a: first number
+ * @b: second number
+ * Return: returns result
+ */
+int mod(int a, int b)
+{
+	return (a%b);
+}
---
+++ 0x18-dynamic_libraries/operation.h
@@ -0,0 +1,12 @@
+#ifndef OPERATION_H
+#define OPERATION_H
+#include <stdio.h>
+#include <string.h>
+
+int add(int, int);
+int sub(int, int);
+int mul(int, int);
+int div(int, int);
+int mod(int, int);
+
+#endif
